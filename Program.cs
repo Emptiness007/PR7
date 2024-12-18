40afb49182491c5e748c6f6a637a4e2a131c538a
emptiness <alenafilimonova98@gmail.com>
2024-12-18 21:06:25 +0500

    Создание метода ParsingHtml. Филимонова

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
@@ -12,6 +14,7 @@ namespace HttpNewsPAT
 {
     internal class Program
     {
+        private static string logFilePath = "debug_log.txt";
         static void Main(string[] args)
         {
             WebRequest request = WebRequest.Create("http://permaviat.ru/main");
@@ -24,15 +27,18 @@ namespace HttpNewsPAT
             reader.Close();
             dataStream.Close();
             response.Close();
+
             CookieContainer cookies = SignIn("student", "Asdfg123");
+
             string content = GetContent(cookies);
-            Console.WriteLine(content);
+            ParsingHtml(content);
+
             Console.Read();
         }
         public static CookieContainer SignIn(string Login, string Password)
         {
             string url = "http://news.permaviat.ru/ajax/login.php";
-            Debug.WriteLine($"Выполняем запрос: {url}");
+            Log($"Выполняем запрос: {url}");
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.Method = "POST";
             request.ContentType = "application/x-www-form-urlencoded";
@@ -46,7 +52,7 @@ namespace HttpNewsPAT
                 stream.Write(Data, 0, Data.Length);
             }
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Debug.WriteLine($"Статус выполнения: {response.StatusCode}");
+            Log($"Статус выполнения: {response.StatusCode}");
             string responseFromServer = new StreamReader(response.GetResponseStream()).ReadToEnd();
             Console.WriteLine(responseFromServer);
             return cookieContainer;
@@ -54,13 +60,35 @@ namespace HttpNewsPAT
         public static string GetContent(CookieContainer cookies)
         {
             string url = "http://news.permaviat.ru/main";
-            Debug.WriteLine($"Выполняем запрос: {url}");
+            Log($"Выполняем запрос: {url}");
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.CookieContainer = cookies;
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Debug.WriteLine($"Статус выполнения: {response.StatusCode}");
+            Log($"Статус выполнения: {response.StatusCode}");
             string responseFromServer = new StreamReader(response.GetResponseStream()).ReadToEnd();
             return responseFromServer;
+        }
+        public static void ParsingHtml(string htmlCode)
+        {
+            var html = new HtmlDocument();
+            html.LoadHtml(htmlCode);
+            var Document = html.DocumentNode;
+            IEnumerable DivsNews = Document.Descendants(0).Where(n => n.HasClass("news"));
+            foreach (HtmlNode DivNews in DivsNews)
+            {
+                var src = DivNews.ChildNodes[1].GetAttributeValue("srs", "none");
+                var name = DivNews.ChildNodes[3].InnerText;
+                var description = DivNews.ChildNodes[5].InnerText;
+                Console.WriteLine(name + "\n" + "Изображение: " + src + "\n" + "Описание: " + description + "\n");
+            }
+        }
+        private static void Log(string message)
+        {
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                writer.WriteLine($"{DateTime.Now}: {message}");
+                Debug.WriteLine(message);
+            }
         }
     }
 }
